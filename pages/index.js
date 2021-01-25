31088075958d76f3aed19c326ecbde0f60ad95a4
Raphael <raphael@agecomnet.com.br>
2021-01-25 17:11:42 -0300

    Alterarando local git

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+      <GitHubCorner projectUrl="https://github.com/raphaom35/Raphaquiz" />
     </QuizBackground>
   );
 }
