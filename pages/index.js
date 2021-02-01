d4dc169cea959ac541c9aa2444457426016ac2b2
Raphael <raphael@agecomnet.com.br>
2021-02-01 20:43:32 -0300

    Aula 05

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,111 @@
 import React from 'react';
 import styled from 'styled-components';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { motion } from "framer-motion";
 import db from '../db.json';
 import Widget from '../src/components/Widget';
-import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
-import Footer from '../src/components/Footer';
-import QuizContainer from '../src/components/QuizContainer';
+import Link from '../src/components/Link';
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
-
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
   const router = useRouter();
   const [name, setName] = React.useState('');
+
   return (
-
     <QuizBackground backgroundImage={db.bg}>
-
+      <Head>
+        <title>{db.title}</title>
+      </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+        as={motion.section}
+        transition={{delay:0,duration:0.5}}
+        variants={{
+          show:{opacity:1, y:'0'},
+          hidden:{opacity:0,y:'100%'}
+        }}
+        initial="hidden"
+        animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (e) {
-              e.preventDefault();
-              router.push(`quiz?name=${name}`);
-              console.log('Fazendo uma submisão por meio do react');
+            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
             }}
             >
               <Input
-                 name="nomeDoUsuario"
-                 onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
-                 placeholder="Diz ai seu nome"
-                 value={name}
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => setName(infosDoEvento.target.value)}
+                placeholder="Diz ai seu nome"
+                value={name}
               />
               <Button type="submit" disabled={name.length === 0}>
-               {`Jogar ${name}`}
+                {`Jogar ${name}`}
               </Button>
             </form>
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget as={motion.section}
+         transition={{delay:0.5,duration:0.5}}
+        variants={{
+          show:{opacity:1},
+          hidden:{opacity:0}
+        }}
+        initial="hidden"
+        animate="show">
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
         <Footer />
